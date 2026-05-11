d532c4604f7c732951162303ef0c6560a3e1ba99
Ansel <anselcolvin@gmail.com>
2026-05-10 18:04:18 -0700

    Hero fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Freeze viewport height at load time so iOS browser chrome show/hide doesn't cause layout shifts
+document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
